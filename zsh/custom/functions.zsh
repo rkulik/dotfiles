166f09e8ed72f8107a0ddad47657aa89e9a6bf2f
René Kulik <rene@kulik.io>
2018-05-10 14:42:09 +0200

    Introduce custom i3 bindings, change aliases and implement upgrade function

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -7,3 +7,20 @@ weather() {
 
     curl "http://wttr.in/$location";
 }
+
+upgrade() {
+    sudo apt-get autoremove
+    echo 'APT auto remove finished...'
+
+    sudo apt-get update
+    echo 'APT update finished...'
+
+    sudo apt-get dist-upgrade
+    echo 'APT distributive upgrade finished...'
+
+    sudo apt-get upgrade
+    echo 'APT upgrade finished...'
+
+    sudo apt-get autoremove
+    echo 'APT auto remove finished...'
+}
