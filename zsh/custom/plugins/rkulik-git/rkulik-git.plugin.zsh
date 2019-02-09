985d6c7f804a30a808a746d09a33afd75342a56d
René Kulik <rene@kulik.io>
2019-02-09 18:03:05 +0100

    Add function for deleting git tags

--- zsh/custom/plugins/rkulik-git/rkulik-git.plugin.zsh
+++ zsh/custom/plugins/rkulik-git/rkulik-git.plugin.zsh
@@ -20,3 +20,18 @@ createTag() {
     git tag -a $1 -m "v$1";
     git push origin HEAD $1;
 }
+
+deleteTag() {
+    if [ ! $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
+        echo "You have to be inside a git repository";
+        return;
+    fi
+
+    if [[ (-z "$1") || ! ($1 =~ ^[0-9]+.[0-9]+.[0-9]+$) ]]; then
+        echo "Tag '$1' does not match [0-9]+.[0-9]+.[0-9]+";
+        return;
+    fi
+
+    git tag -d $1;
+    git push origin :refs/tags/$1;
+}
